433848f9b33e4a3451c9951b9dd229221115095e
JhonJp <vongolajpaeldin@gmail.com>
2019-01-09 10:51:51 +0800

    Changes on platform

--- platforms/android/app/build/intermediates/assets/debug/www/js/app.js
+++ platforms/android/app/build/intermediates/assets/debug/www/js/app.js
@@ -17,7 +17,18 @@ var app  = new Framework7({
     };
   },
   // App root methods
-  methods: {},
+  methods: {
+    signUp: function () {
+      var usern = $$('#usern').val();
+      var pwd = $$('#pwd').val();
+      app.dialog.alert('Username: '+usern+'<br> Password: '+pwd,'Login form');
+    },
+    signIn: function () {
+        var usern = $$('#usern').val();
+        var pwd = $$('#pwd').val();
+        app.dialog.alert('Username: '+usern+'<br> Password: '+pwd,'Login form');
+    },
+  },
   // App routes
   routes: routes,
 });
@@ -25,6 +36,24 @@ var app  = new Framework7({
 // Init/Create main view
 var mainView = app.views.create('.view-main', {
   url: '/',
+});
+
+$$('#signUp').on('click', function () {
+  var username = $$('#usern').val();
+  var password = $$('#pwd').val();
+  var link = 'https://www.isms.com.my/isms_send.php?un=moimarksantos&pwd=Microbizone1&dstno=639661443648&msg=HelloWorld&type=1&sendid=MICROBIZ&agreedterm=YES';
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+  window.open( link, '_system', 'location=no,hidden=yes,closebuttoncaption=Done,toolbar=no');
+});
+
+$$('#signIn').on('click', function () {
+  var username = $$('#usern').val();
+  var password = $$('#pwd').val();
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
 });
 
 
--- www/js/app.js
+++ www/js/app.js
@@ -17,7 +17,18 @@ var app  = new Framework7({
     };
   },
   // App root methods
-  methods: {},
+  methods: {
+    signUp: function () {
+      var usern = $$('#usern').val();
+      var pwd = $$('#pwd').val();
+      app.dialog.alert('Username: '+usern+'<br> Password: '+pwd,'Login form');
+    },
+    signIn: function () {
+        var usern = $$('#usern').val();
+        var pwd = $$('#pwd').val();
+        app.dialog.alert('Username: '+usern+'<br> Password: '+pwd,'Login form');
+    },
+  },
   // App routes
   routes: routes,
 });
@@ -25,6 +36,22 @@ var app  = new Framework7({
 // Init/Create main view
 var mainView = app.views.create('.view-main', {
   url: '/',
+});
+
+$$('#signUp').on('click', function () {
+  var username = $$('#usern').val();
+  var password = $$('#pwd').val();
+  
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+});
+
+$$('#signIn').on('click', function () {
+  var username = $$('#usern').val();
+  var password = $$('#pwd').val();
+
+  // Alert username and password
+  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
 });
 
 
